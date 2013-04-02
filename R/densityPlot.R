05313fb263b7b180bc0ac1bceef996b038ff931e
Mike Meredith <mmeredith@wcsmsia>
2013-04-02 19:38:01 +0800

    Added vignette and overlapPlot

--- R/densityPlot.R
+++ R/densityPlot.R
@@ -1,33 +1,38 @@
 densityPlot <-
-function(A, xscale=24, plot=TRUE, add=FALSE, rug=FALSE,
+function(A, xscale=24, add=FALSE, rug=FALSE,
     n.grid=128, kmax = 3, c = 1, ...)  {
 
   bw <- getBandWidth(A, kmax=kmax) * c
   if(is.na(bw))
     stop("Bandwidth estimation failed.")
-  xx <- seq(0, 2*pi, length=n.grid)
+  # xx <- seq(0, 2*pi, length=n.grid)
+  xx <- seq(-pi/4, 9*pi/4, length=n.grid)
   densA <- densityFit(A, xx, bw)
   xsc <- if(is.na(xscale)) 1 else xscale / (2*pi)
   toPlot <- cbind(x = xx * xsc, y = densA / xsc)
   
-  if(plot) {
-    if(add)  {
-      lines(toPlot, ...)
+  if(!add)  {
+    plot(toPlot, type='n', #las=1, 
+      ylim=c(0, max(toPlot[,'y'])), xlab="Time", ylab="Density", xaxt='n', ...)
+    if(is.na(xscale)) {
+      axis(1, at=c(0, pi/2, pi, 3*pi/2, 2*pi),
+        labels=c("0", expression(pi/2), expression(pi),
+          expression(3*pi/2), expression(2*pi)))
+    } else if(xscale == 24) {
+      axis(1, at=c(0,6,12,18,24),
+        labels=c("0:00", "6:00", "12:00", "18:00", "24:00"))
     } else {
-      plot(toPlot, type='l', las=1, xlab="Time", ylab="Density", xaxt='n', ...)
-      if(is.na(xscale)) {
-        axis(1, at=c(0, pi/2, pi, 3*pi/2, 2*pi),
-          labels=c("0", expression(pi/2), expression(pi),
-            expression(3*pi/2), expression(2*pi)))
-      } else if(xscale == 24) {
-        axis(1, at=c(0,6,12,18,24),
-          labels=c("0:00", "6:00", "12:00", "18:00", "24:00"))
-      } else {
-        axis(1)
-      }
+      axis(1)
     }
-    if(rug)
-      rug(A * xsc, ...)
+    abline(h=0, col='grey')
+    midnt <- c(0, 2*pi) * xsc
+    edge <- par('usr')
+    rect(c(edge[1], midnt[2]), rep(edge[3], 2), c(midnt[1], edge[2]), rep(edge[4],2),
+      border=NA, col='lightgrey')
+    box()
   }
+  lines(toPlot, ...)
+  if(rug)
+    rug(A * xsc, ...)
   return(invisible(toPlot))
 }
---
+++ R/overlapPlot.R
@@ -0,0 +1,34 @@
+overlapPlot <-
+function(A, B, xscale=24, linetype=c(1, 2), linecol=c('black', 'blue'),
+    olapcol='lightgrey', n.grid=128, kmax = 3, c = 1, ...)  {
+  # does a nice plot of two density curves with overlap shaded
+  bwA <- getBandWidth(A, kmax=kmax) * c
+  bwB <- getBandWidth(B, kmax=kmax) * c
+  if(is.na(bwA) || is.na(bwB))
+    stop("Bandwidth estimation failed.")
+  xsc <- if(is.na(xscale)) 1 else xscale / (2*pi)
+  xxRad <- seq(0, 2*pi, length=n.grid)
+  xx <- xxRad * xsc
+  densA <- densityFit(A, xxRad, bwA) / xsc
+  densB <- densityFit(B, xxRad, bwB) / xsc
+  densOL <- pmin(densA, densB)
+  ylim <- c(0, max(densA, densB))
+  #toPlot <- cbind(x = xx * xsc, y = densA / xsc)
+  
+  plot(0, 0, type='n', #las=1, 
+    ylim=ylim, xlim=range(xx), xlab="Time", ylab="Density", xaxt='n', ...)
+  if(is.na(xscale)) {
+    axis(1, at=c(0, pi/2, pi, 3*pi/2, 2*pi),
+      labels=c("0", expression(pi/2), expression(pi),
+        expression(3*pi/2), expression(2*pi)))
+  } else if(xscale == 24) {
+    axis(1, at=c(0,6,12,18,24),
+      labels=c("0:00", "6:00", "12:00", "18:00", "24:00"))
+  } else {
+    axis(1)
+  }
+  polygon(c(c(2*pi, 0), xx), c(0, 0, densOL), border=NA, col=olapcol)
+  lines(xx, densA, lty=linetype[1], col=linecol[1]) 
+  lines(xx, densB, lty=linetype[2], col=linecol[2]) 
+  return(invisible(list(x = xx, densityA = densA, densityB = densB)))
+}
