ba6f45c69f712e7ffbeef6ee9196fbeada3d5982
Mike Meredith <mmeredith@wcsmsia>
2017-05-22 12:13:23 +0800

    Added parallel processing to bootEst

--- R/bootEst.R
+++ R/bootEst.R
@@ -1,32 +1,72 @@
 # Calculate a series of estimates of overlap based on bootstrap samples.
 
-# Amat, Bmat: matrices with bootstrap samples in columns
-# kmax: maximum value of k for optimal bandwidth estimation
-# adjust: smoothing adjustment, a vector of length 3 for
-#   Dhat1, Dhat4, Dhat5; if NA, the corresponding estimator is
-#   ommitted. (adjust = 1/c in old code)
-# n.grid: number of points at which to estimate density
+# Parallel processing added in v. 0.2.7.9002 (2017-05-22)
 
-# Returns: a matrix of overlap estimates with a column for
-#   each estimator
+# Function to determine the number of iterations for each core
+# ============================================================
+workLoad <- function(iterations, cores) {
+  min <- iterations %/% cores
+  xtra <- iterations - min*cores
+  return( c(rep(min+1, xtra), rep(min, cores - xtra)) )
+}
 
 
-bootEst <-
-function(Amat, Bmat, kmax=3, adjust=c(0.8, 1, 4), n.grid=128,
-      type=c("all", "Dhat1", "Dhat4", "Dhat5")) {
-  nboot <- min(ncol(Amat), ncol(Bmat))
-  type <- match.arg(type)
+# Function to do bootstraps on a single core
+# ==========================================
+#   dataList is a list with the matrices for the 2 species
+bootEstSerial <- function(dataList, kmax, adjust, n.grid, type) {
+  nboot <- min(sapply(dataList, ncol))
   if(type == "all") {
     bsamp <- matrix(NA, nboot, 3)
-    colnames(bsamp) <- c("Dhat1", "Dhat4", "Dhat5")
   } else {
     bsamp <- matrix(NA, nboot, 1)
   }
   
   for(i in 1:nboot)
-    bsamp[i, ] <- overlapEst(Amat[, i], Bmat[, i], kmax=kmax,
-                          adjust=adjust, n.grid=n.grid, type=type) 
-  if(ncol(bsamp) == 1)
-    bsamp <- as.vector(bsamp)
+    bsamp[i, ] <- overlapEst(dataList[[1]][, i], dataList[[2]][, i],
+                kmax=kmax, adjust=adjust, n.grid=n.grid, type=type) 
   return(bsamp)
+}
+
+# Main function
+# =======================================
+bootEst <-
+function(Amat, Bmat, kmax=3, adjust=c(0.8, 1, 4), n.grid=128,
+      type=c("all", "Dhat1", "Dhat4", "Dhat5"), cores=1) {
+  type <- match.arg(type)
+  
+  # Deal with the parallel stuff:
+  if(is.na(cores))
+    cores <- parallel::detectCores() - 1
+  if(cores > 1) {
+    cl <- makeCluster(cores) ; on.exit(stopCluster(cl))
+    clusterEvalQ(cl, library(overlap))
+    # split the matrices into chunks for each worker:
+    nboot <- min(ncol(Amat), ncol(Bmat))
+    workerID <- rep(1:cores, workLoad(nboot, cores))
+    dataList <- vector("list", cores)
+    for(i in 1:cores)
+      dataList[[i]] <- list(Amat[, workerID==i], Bmat[, workerID==i])
+
+    # Run the thing
+    resList <- parLapply(cl, dataList, bootEstSerial,
+            kmax=kmax, adjust=adjust, n.grid=n.grid, type=type)
+    bsamp <- do.call(rbind, resList) 
+  } else {
+    bsamp <- bootEstSerial(list(Amat, Bmat),
+        kmax=kmax, adjust=adjust, n.grid=n.grid, type=type)
+  }
+  if(ncol(bsamp) == 1 && type != "all")  # simple, fast
+    return(as.vector(bsamp))
+  if(ncol(bsamp) < 3 && type == "all") { # messy! Must be NAs in 'adjust'
+    nonNA <- which(!is.na(adjust))
+    stopifnot(ncol(bsamp) == length(nonNA))
+    out <- matrix(NA, nboot, 3)
+    for(i in 1:ncol(bsamp))
+      out[, nonNA[i]] <- bsamp[, i]
+  } else {
+    out <- bsamp
+  }
+  colnames(out) <- c("Dhat1", "Dhat4", "Dhat5")
+  return(out)
 }
--- R/overlapEst.R
+++ R/overlapEst.R
@@ -15,13 +15,14 @@ function( A, B, kmax=3, adjust=c(0.8, 1, 4), n.grid=128,
 
   if(length(adjust) == 1)
     adjust <- rep(adjust, 3)
+  adj <- adjust
   type <- match.arg(type)
   index <- switch(type,
             all = 0,
-            Dhat1 = {is.na(adjust) <- 2:3 ; 1},
-            Dhat4 = {is.na(adjust) <- c(1,3) ; 2},
-            Dhat5 = {is.na(adjust) <- 1:2 ; 3} )
-  if(index && is.na(adjust[index]))
+            Dhat1 = {is.na(adj) <- 2:3 ; 1},
+            Dhat4 = {is.na(adj) <- c(1,3) ; 2},
+            Dhat5 = {is.na(adj) <- 1:2 ; 3} )
+  if(index && is.na(adj[index]))
     stop("You specified adjust = NA for the selected type: ", type)
   
   grid <- seq(0, 2*pi, length=n.grid)
@@ -32,10 +33,10 @@ function( A, B, kmax=3, adjust=c(0.8, 1, 4), n.grid=128,
   bw0.A <- getBandWidth(A, kmax=kmax)
   bw0.B <- getBandWidth(B, kmax=kmax)
   if(!is.na(bw0.A) && !is.na(bw0.B))  {  # if no Uniroot Error
-    if(!is.na(adjust[1]))  {  # Do Dhat1
+    if(!is.na(adj[1]))  {  # Do Dhat1
       dA.current <- 1
-      dA <- densityFit(A, grid, bw0.A / adjust[1])
-      dB <- densityFit(B, grid, bw0.B / adjust[1])
+      dA <- densityFit(A, grid, bw0.A / adj[1])
+      dB <- densityFit(B, grid, bw0.B / adj[1])
       # Prune first value and scale to sum to 1
       dA1 <- dA[-1] / sum(dA[-1])
       dB1 <- dB[-1] / sum(dB[-1])
@@ -45,42 +46,42 @@ function( A, B, kmax=3, adjust=c(0.8, 1, 4), n.grid=128,
     n1 <- length(A)
     allObs <- c(A, B)
     n <- length(allObs)
-    if(!is.na(adjust[2]))  {   # Get kernel densities for Dhat4
+    if(!is.na(adj[2]))  {   # Get kernel densities for Dhat4
       if(n > (n.grid*1.1)) {  # Use interpolation
-        if(is.na(adjust[1]) | !identical(all.equal(adjust[1], adjust[2]), TRUE))  { 
+        if(is.na(adj[1]) | !identical(all.equal(adj[1], adj[2]), TRUE))  { 
           dA.current <- 2
-          dA <- densityFit(A, grid, bw0.A / adjust[2])
-          dB <- densityFit(B, grid, bw0.B / adjust[2])
+          dA <- densityFit(A, grid, bw0.A / adj[2])
+          dB <- densityFit(B, grid, bw0.B / adj[2])
         }
         dAx <- approx(grid, dA, allObs)$y
         dBx <- approx(grid, dB, allObs)$y
       } else {
-        dAx <- densityFit(A, allObs, bw0.A / adjust[2])
-        dBx <- densityFit(B, allObs, bw0.B / adjust[2])
+        dAx <- densityFit(A, allObs, bw0.A / adj[2])
+        dBx <- densityFit(B, allObs, bw0.B / adj[2])
       }
       out[2] <- (mean(pmin(1, dBx[1:n1] / dAx[1:n1])) +   # Dhat4
                  mean(pmin(1, dAx[(n1+1):n] / dBx[(n1+1):n]))) / 2
     }
-    if(!is.na(adjust[3]))  {
-      if(is.na(adjust[2]) | !identical(all.equal(adjust[2], adjust[3]), TRUE))  { 
+    if(!is.na(adj[3]))  {
+      if(is.na(adj[2]) | !identical(all.equal(adj[2], adj[3]), TRUE))  { 
         # Do new kernel densities for Dhat5
         if(n > (n.grid*1.1)) {  # Use interpolation
-          if(is.na(dA.current) | !identical(all.equal(adjust[dA.current], adjust[3]), TRUE))  { 
-            dA <- densityFit(A, grid, bw0.A / adjust[3])
-            dB <- densityFit(B, grid, bw0.B / adjust[3])
+          if(is.na(dA.current) | !identical(all.equal(adj[dA.current], adj[3]), TRUE))  { 
+            dA <- densityFit(A, grid, bw0.A / adj[3])
+            dB <- densityFit(B, grid, bw0.B / adj[3])
           }
           dAx <- approx(grid, dA, allObs)$y
           dBx <- approx(grid, dB, allObs)$y
         } else {
-          dAx <- densityFit(A, allObs, bw0.A / adjust[3])
-          dBx <- densityFit(B, allObs, bw0.B / adjust[3])
+          dAx <- densityFit(A, allObs, bw0.A / adj[3])
+          dBx <- densityFit(B, allObs, bw0.B / adj[3])
         }
       }
       out[3] <- mean(dBx[1:n1] > dAx[1:n1]) +             # Dhat5
                 mean(dAx[(n1+1):n] >= dBx[(n1+1):n])
     }
   }  # end no uniroot error 
-  if(index)
+  if(type != "all" && index)
     out <- out[index]
   return(out)
 }
